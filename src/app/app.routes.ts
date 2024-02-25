bc9eded915738602a44caa90db9bf6d33137eedb
dar0x <axel.darox.ec@gmail.com>
2024-02-24 23:16:26 -0500

    feat: necessary components are added and routes are configured

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'pokemon',
+    loadChildren: () => import('./modules/pokemon/pokemon.module').then(m => m.PokemonModule)
+  },
+];
---
+++ src/app/modules/pokemon/pokemon-routing.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { PokemonListComponent } from './pokemon-list/pokemon-list.component';
+import { PokemonDetailComponent } from './pokemon-detail/pokemon-detail.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: PokemonListComponent
+  },
+  {
+    path: 'detail/:id',
+    component: PokemonDetailComponent
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class PokemonRoutingModule {
+}
